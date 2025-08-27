1c89abe038747e40efc222b25bad4e8f6bfc0140
Andrei <suretrick@gmail.com>
2025-08-27 19:49:21 +0300

    add sql-inj index.php

---
+++ sql-inj-index.php
@@ -0,0 +1,20 @@
+// Это index.php для SQL-injection
+
+<?php
+$conn = pg_connect("host=localhost port=5432 dbname=dbms1 user=postgres password=postgres");
+
+$first_name = $_GET['first_name'] ?? '';
+$query = "SELECT * FROM persons WHERE first_name = '$first_name'";  // небезопасно для обучения
+
+$result = pg_query($conn, $query);
+
+while($row = pg_fetch_assoc($result)){
+    echo $row['first_name'] . " " . $row['last_name'] . " (" . $row['age'] . ")<br>";
+}
+?>
+
+
+
+// curl "http://localhost/testapp/index.php?first_name=John"
+// curl "http://localhost/testapp/index.php?first_name=John%27%20OR%20%271%27%3D%271"
+// curl "http://localhost/testapp/index.php?first_name=$(python3 -c "import urllib.parse, sys; print(urllib.parse.quote(sys.argv[1]))" "John' OR '1'='1")"
