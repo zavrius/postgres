44e2ed5bd1958ef48395a045609c7fa603075dd8
Andrei <suretrick@gmail.com>
2025-10-31 07:48:35 +0300

    Create ms2.sql

---
+++ ms2.sql
@@ -0,0 +1,93 @@
+/* Данный скрипт разработан
+ * в компании Диасофт
+ * для предварительного показа функционала 
+ * продукта Digital Q.DataBase
+ * в режиме эмуляции СУБД Microsoft SQL Server */
+
+-- Меняем БД на demo в стиле MS SQL
+USE ms_test1
+GO
+
+-- Установка формата дат и получение id-соединения - все в стиле MS SQL
+SET dateformat dmy
+GO
+select @@spid
+GO
+-- никаких "точек с запятой", использование конструкции GO - это все также характерно для MS SQL
+
+
+
+--Функции из MS SQL, которые часто применяются в запросах и отсутствуют в обычном PostgreSQL
+SELECT DATEDIFF(DAY, '01.01.2024', '01.01.2025')
+GO
+SELECT DATEADD(DAY, 5, '01.01.2025')
+GO
+
+-- Удалим таблицу если она есть, заодно проверим функции работы с метаданными из MS SQL 
+IF OBJECT_ID ( 'AUTO_PK_SUPPORT', 'U' ) IS NOT NULL
+    DROP TABLE AUTO_PK_SUPPORT
+GO
+
+-- Создадим таблицу    
+CREATE TABLE AUTO_PK_SUPPORT (
+ TABLE_NAME   VARCHAR(18) NOT NULL,   
+ NEXT_ID   INTEGER DEFAULT 0 NOT NULL   
+)
+GO
+
+-- Удалим процедуру если она есть 
+IF OBJECT_ID ( 'AUTO_PK_GEN', 'P' ) IS NOT NULL
+    DROP PROCEDURE AUTO_PK_GEN
+GO
+
+-- Создадим хранимую процедуру используя "родной" для MS SQL синтаксис
+-- Обычный PostgreSQL такое никогда исполнить не сможет!     
+CREATE PROCEDURE AUTO_PK_GEN
+            @TABLENAME VARCHAR(18),
+            @BATCHSIZE INTEGER,
+            @NEXT_ID INTEGER OUT
+            AS
+BEGIN
+     BEGIN TRANSACTION
+        UPDATE AUTO_PK_SUPPORT SET NEXT_ID = NEXT_ID + @BATCHSIZE
+        WHERE TABLE_NAME = @TABLENAME
+
+        IF @@ROWCOUNT = 0
+        INSERT INTO AUTO_PK_SUPPORT (TABLE_NAME, NEXT_ID)
+        VALUES (@TABLENAME, 1000)
+       
+        SET @NEXT_ID = (SELECT NEXT_ID FROM AUTO_PK_SUPPORT WHERE TABLE_NAME = @TABLENAME)
+     COMMIT
+END
+GO
+
+-- Удалим функцию если она есть 
+IF OBJECT_ID ( 'AUTO_PK_FOR_TABLE', 'P' ) IS NOT NULL
+    DROP PROCEDURE AUTO_PK_FOR_TABLE
+GO
+
+-- Создадим функцию используя "родной" для MS SQL синтаксис 
+CREATE PROCEDURE AUTO_PK_FOR_TABLE
+ @TNAME VARCHAR(18),
+ @PKBATCHSIZE INTEGER
+ AS
+BEGIN
+ DECLARE @NEXT_ID INTEGER
+ UPDATE AUTO_PK_SUPPORT SET @NEXT_ID = NEXT_ID = NEXT_ID + @PKBATCHSIZE
+  WHERE TABLE_NAME = @TNAME
+ SELECT @NEXT_ID as NEXT_ID
+END
+GO
+
+
+
+-- Позовем процедуру, один из параметров - переменная для возврата значений OUTPUT-параметра 
+DECLARE @pk_value int
+EXEC dbo.[AUTO_PK_GEN] @TABLENAME = 'BOOKS', @BATCHSIZE = 10, @NEXT_ID = @pk_value OUTPUT 
+-- В этом exec есть обращение к ролям и квотирование идентификаторов в стиле MS SQL
+SELECT @pk_value as RES
+GO
+
+-- Вызовем функцию
+EXECUTE AUTO_PK_FOR_TABLE @TNAME = N'BOOKS', @PKBATCHSIZE = 1
+GO
